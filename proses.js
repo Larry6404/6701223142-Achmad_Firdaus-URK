8de6f991be4c729547e0490f17e08a5f93d9a184
Muhammad Mikail Gabril <114597845+mikail6404@users.noreply.github.com>
2024-08-10 16:55:07 +0700

    V1.4 Merge Branch-Nim to Main

--- proses.js
+++ proses.js
@@ -22,33 +22,3 @@ document.getElementById('calculatorForm').addEventListener('submit', function (e
     // Tampilkan hasil
     document.getElementById('result').textContent = `Hasil: ${result}`;
 });
-
-document.getElementById('calculatorForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    // Ambil nilai dari form input
-    let number1 = parseFloat(document.getElementById('number1').value);
-    let operator = document.getElementById('operator').value;
-    let number2 = parseFloat(document.getElementById('number2').value);
-
-    let result;
-
-    // Lakukan operasi aritmatika menggunakan switch case
-    switch (operator) {
-        case '+':
-            result = number1 + number2;
-            break;
-        case '-':
-            result = number1 - number2;
-            break;
-        case '*':
-            result = number1 * number2;
-            break;
-        default:
-            result = 'Operator tidak dikenali';
-            break;
-    }
-
-    // Tampilkan hasil
-    document.getElementById('result').textContent = `Hasil: ${result}`;
-});
